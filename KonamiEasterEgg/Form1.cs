bfde24b915db1fdcc4f5ee04818e7afbee7bec12
TobiasVdb <tobiasvandenbempt@hotmail.com>
2014-06-24 13:12:14 +0200

    Konami Code Detector

---
+++ KonamiEasterEgg/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonamiEasterEgg
+{
+    public partial class Form1 : Form
+    {
+        private KonamiEasterEggNotifier _notifier;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _notifier = new KonamiEasterEggNotifier();
+            _notifier.KonamiSequenceEntered += _notifier_KonamiSequenceEntered;
+            label1.Visible = false;
+        }
+
+        void _notifier_KonamiSequenceEntered(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _notifier.KeyPressed(e.KeyChar);
+            label1.Visible = _notifier.GetSequenceStatus();
+        }
+
+        private void label1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            Notify(e.KeyCode);
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            Notify(e.KeyCode);
+        }
+
+        private void Notify(Keys keyCode)
+        {
+            switch (keyCode)
+            {
+                case Keys.Down:
+                    _notifier.KeyPressed('D');
+                    break;
+                case Keys.Right:
+                    _notifier.KeyPressed('R');
+                    break;
+                case Keys.Up:
+                    _notifier.KeyPressed('U');
+                    break;
+                case Keys.Left:
+                    _notifier.KeyPressed('L');
+                    break;
+            }
+
+            label1.Visible = _notifier.GetSequenceStatus();
+        }
+    }
+}
---
+++ KonamiEasterEgg/KonamiEasterEggNotifier.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonamiEasterEgg
+{
+    public class KonamiEasterEggNotifier
+    {
+        private List<char> _keyList;
+        public event EventHandler KonamiSequenceEntered;
+        public KonamiEasterEggNotifier()
+        {
+            _keyList = new List<char>();
+        }
+
+        private void OnKonamiSequenceDetected()
+        {
+            EventHandler handler = KonamiSequenceEntered;
+            if (null != handler) handler(this, EventArgs.Empty);
+        }
+
+        public void KeyPressed(char key)
+        {
+            //Track pressed keys, space is clear!
+            if(SequenceChecks(key))//Do sequence checks here
+                OnKonamiSequenceDetected();
+        }
+
+        public bool GetSequenceStatus()
+        {
+            return SequenceChecks(null);
+        }
+
+        private bool SequenceChecks(char? key)
+        {
+            if (key != null)
+            {
+                if (key.Value == (char)Keys.Space)
+                    _keyList.Clear();
+                else
+                    _keyList.Add(key.Value);
+            }
+            //Konami Code ↑ ↑ ↓ ↓ ← → ← → B A Enter
+            var correctSequence = true;
+            if (_keyList.Count == 1) correctSequence = correctSequence && _keyList[0] == 'U';
+            if(_keyList.Count == 2) correctSequence = correctSequence && _keyList[1] == 'U';
+            if(_keyList.Count == 3) correctSequence = correctSequence && _keyList[2] == 'D';
+            if(_keyList.Count == 4) correctSequence = correctSequence && _keyList[3] == 'D';
+            if(_keyList.Count == 5) correctSequence = correctSequence && _keyList[4] == 'L';
+            if(_keyList.Count == 6) correctSequence = correctSequence && _keyList[5] == 'R';
+            if(_keyList.Count == 7) correctSequence = correctSequence && _keyList[6] == 'L';
+            if(_keyList.Count == 8) correctSequence = correctSequence && _keyList[7] == 'R';
+            if(_keyList.Count == 9) correctSequence = correctSequence && _keyList[8] == 'B';
+            if(_keyList.Count == 10) correctSequence = correctSequence && _keyList[9] == 'A';
+            if(_keyList.Count == 11) 
+                correctSequence = correctSequence && _keyList[10] == '\r';
+
+            return (_keyList.Count == 11) && correctSequence;
+        }
+
+        public void KeyPressed(PreviewKeyDownEventArgs keyPressEventArgs)
+        {
+            if (SequenceChecks((char)keyPressEventArgs.KeyCode))//Do sequence checks here
+                OnKonamiSequenceDetected();
+        }
+    }
+}
